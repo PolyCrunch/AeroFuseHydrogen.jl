b6c50e9eeb319763ac35f79f732ffc509fd6b4f0
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2024-11-17 16:53:18 +0000

    Initial commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "AeroFuseHydrogen.jl" begin
     # Write your tests here.
+    @test YourPackageName.greet_your_package_name() == "Hello AeroFuseHydrogen"
+    @test YourPackageName.greet_your_package_name() != "Hello world!"
 end
