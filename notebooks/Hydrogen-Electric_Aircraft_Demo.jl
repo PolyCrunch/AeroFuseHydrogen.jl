4fd3efc5d950014b65c7e1a64462f298e594f837
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2024-11-17 17:17:48 +0000

    Added a demo notebook

---
+++ notebooks/Hydrogen-Electric_Aircraft_Demo.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.19.46
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 316a98fa-f3e4-4b46-8c19-c5dbfa6a550f
+
+
+# ╔═╡ 9ec9f734-a507-11ef-184e-cfebb9cf6885
+
+
+# ╔═╡ Cell order:
+# ╠═316a98fa-f3e4-4b46-8c19-c5dbfa6a550f
+# ╠═9ec9f734-a507-11ef-184e-cfebb9cf6885
