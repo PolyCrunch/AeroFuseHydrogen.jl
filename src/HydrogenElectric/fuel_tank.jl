b47622d31ea85f6f6ad09ebce0fb26b4cdcd0fa6
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2024-11-28 18:43:10 +0000

    Formatter on fuel_tank.jl

--- src/HydrogenElectric/fuel_tank.jl
+++ src/HydrogenElectric/fuel_tank.jl
@@ -1,10 +1,10 @@
-struct CryogenicFuelTank{T <: Real, N <: AbstractAffineMap} <: AbstractFuelTank
-    internal_volume :: T
-    wall_thickness  :: T
-    radius          :: T
-    length          :: T
-    mass            :: T
-    affine          :: N
+struct CryogenicFuelTank{T<:Real,N<:AbstractAffineMap} <: AbstractFuelTank
+    internal_volume::T
+    wall_thickness::T
+    radius::T
+    length::T
+    mass::T
+    affine::N
     function CryogenicFuelTank(R, V_internal, t_wall)
         # Type promotion
         T = promote_type(eltype(R), eltype(V_internal), eltype(t_wall))
@@ -14,22 +14,22 @@ struct CryogenicFuelTank{T <: Real, N <: AbstractAffineMap} <: AbstractFuelTank
         @assert V_internal > 0 "Internal volume must be positive"
         @assert t_wall > 0 "Wall thickness must be positive"
 
-        new{T, N}(R, V_internal, t_wall)
+        new{T,N}(R, V_internal, t_wall)
     end
 end
 
 function CryogenicFuelTank(;
-        internal_volume = 1.,
-        wall_thickness  = 0.05,
-        radius          = 1.,
-        position        = zeros(3),
-        angle           = 0.,
-        axis            = [0., 1., 0.],
-        affine          = AffineMap(AngleAxis(deg2rad(angle), axis...), SVector(position...)),
-    )
+    internal_volume=1.0,
+    wall_thickness=0.05,
+    radius=1.0,
+    position=zeros(3),
+    angle=0.0,
+    axis=[0.0, 1.0, 0.0],
+    affine=AffineMap(AngleAxis(deg2rad(angle), axis...), SVector(position...)),
+)
 
-    length = 1.
-    mass = 1.
+    length = 1.0
+    mass = 1.0
 
     return CryogenicFuelTank(internal_volume, wall_thickness, radius, length, mass, affine)
 end
