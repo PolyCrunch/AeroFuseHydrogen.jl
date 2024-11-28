ca3f1db6cc52dcea91ad6e771445f3db1a7e4be1
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2024-11-28 19:33:12 +0000

    Maybe actually fixed test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,5 @@ using Test
     # Write your tests here.
     @test AeroFuseHydrogen.greet_AeroFuse() == "Hello AeroFuseHydrogen"
     @test AeroFuseHydrogen.greet_AeroFuse() != "Hello world!"
-    @test typeof(AeroFuseHydrogen.CryogenicFuelTank()) == CryogenicFuelTank
+    @test AeroFuseHydrogen.CryogenicFuelTank() isa CryogenicFuelTank
 end
