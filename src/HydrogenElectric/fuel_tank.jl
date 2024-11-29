17097d98664dfee74a0df8981164f0d4492f46ec
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2024-11-29 15:23:32 +0000

    Hopefully the length function works now

--- src/HydrogenElectric/fuel_tank.jl
+++ src/HydrogenElectric/fuel_tank.jl
@@ -4,17 +4,17 @@ struct CryogenicFuelTank{T<:Real,N<:AbstractAffineMap} <: AbstractFuelTank
     internal_volume::T
     insulation_thickness::T
     affine::N
-    # length::T
-    # mass::T
+    length::T
+    mass::T
     function CryogenicFuelTank(R, V_internal, t_wall, affine)
         # Type promotion
         T = promote_type(eltype(R), eltype(V_internal), eltype(t_wall))
         N = typeof(affine)
-        
+
         @assert R > 0 "Radius must be positive"
         @assert V_internal > 0 "Internal volume must be positive"
         @assert t_wall > 0 "Wall thickness must be positive"
-        
+
         new{T,N}(R, V_internal, t_wall, affine)
     end
 end
@@ -27,9 +27,9 @@ function CryogenicFuelTank(;
     angle=0.0,
     axis=[0.0, 1.0, 0.0],
     affine=AffineMap(AngleAxis(deg2rad(angle), axis...), SVector(position...)),
-    )
-    
+)
+
     return CryogenicFuelTank(radius, internal_volume, insulation_thickness, affine)
 end
 
-Base.length(fuel_tank :: CryogenicFuelTank) = fuel_tank.internal_volume / (π * (fuel_tank.radius - fuek_tank.insulation_thickness)^2) + 2 / 3 * fuel_tank.radius + 4 / 3 * fuel_tank.insulation_thickness
+length(fuel_tank::CryogenicFuelTank) = fuel_tank.internal_volume / (π * (fuel_tank.radius - fuek_tank.insulation_thickness)^2) + 2 / 3 * fuel_tank.radius + 4 / 3 * fuel_tank.insulation_thickness
