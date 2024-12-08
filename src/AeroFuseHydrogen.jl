5a76f7bfa828269a69b45e8e9509de54aa86b165
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2024-12-08 19:06:39 +0000

    AAAAAA

--- src/AeroFuseHydrogen.jl
+++ src/AeroFuseHydrogen.jl
@@ -21,15 +21,25 @@ import .HydrogenElectric: AbstractHydrogenPropulsionSystem, AbstractFuelTank, Ab
 export AbstractHydrogenPropulsionSystem, AbstractFuelTank, AbstractEngine, AbstractPropeller
 
 # Fuel tank
-import .HydrogenElectric: CryogenicFuelTank, volume_to_length, internal_volume, dry_mass, wet_mass, boil_off
+import .HydrogenElectric: CryogenicFuelTank, volume_to_length, internal_volume, dry_mass, wet_mass
 
-export CryogenicFuelTank, volume_to_length, internal_volume, dry_mass, wet_mass, boil_off
+export CryogenicFuelTank, volume_to_length, internal_volume, dry_mass, wet_mass
+
+# Boil-off
+import .HydrogenElectric: tank_surface_temperature, boil_off
+
+export tank_surface_temperature, boil_off
 
 include("Tools/Tools.jl")
 
 # Data import
-import .Tools: read_data, K_air, α_air, ν_air, tank_surface_temperature, boil_off
+import .Tools: read_data
 
-export read_data, K_air, α_air, ν_air, tank_surface_temperature, boil_off
+export read_data
+
+# Gas properties
+import .Tools: K_air, α_air, ν_air
+
+export K_air, α_air, ν_air
 
 end
