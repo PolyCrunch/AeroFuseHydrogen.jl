e7e2a19c66d25d46bb8e2d3e40c0cc409196eb64
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2025-02-03 17:59:35 +0000

    Changes default number of sections and circle points

--- src/Tools/plot_tools.jl
+++ src/Tools/plot_tools.jl
@@ -1,6 +1,5 @@
 # Fuel tank
-function plot_surface(tank::CryogenicFuelTank, n_secs=5)
-    n_pts = 20
+function plot_surface(tank::CryogenicFuelTank, n_secs=5, n_pts=10)
     xys = cosine_interpolation(tank, n_secs)
 
     circs = combinedimsview(
@@ -23,8 +22,8 @@ end
 ## Plots.jl recipes
 #============================================#
 
-@recipe function tank_plot(tank::FuelTank, n=20)
-    tank_pans = plot_surface(tank, n)
+@recipe function tank_plot(tank::FuelTank, n_secs=5, n_circ=10)
+    tank_pans = plot_surface(tank, n_secs, n_circ)
 
     for k in axes(tank_pans, 3)[1:end-1]
         for n in axes(tank_pans, 1)[1:end-1]
@@ -40,12 +39,12 @@ end
     end
 end
 
-@recipe function tank_plot(tank :: CryogenicFuelTank; n_secs = 10, n_circ = 20)
+@recipe function tank_plot(tank::CryogenicFuelTank; n_secs=5, n_circ=10)
     ts = LinRange(0, 1, n_secs)
     tank_coo = coordinates(tank, ts, n_circ) # Get coordinates
-    tank_ske = [ tank_coo[end÷4,:,1:3]; tank_coo[end÷2,:,1:3]; tank_coo[3*end÷4,:,1:3] ] 
-    tank_plan = [ reshape(tank_coo, n_circ * n_secs * 3, 3); tank_ske ]
+    tank_ske = [tank_coo[end÷4, :, 1:3]; tank_coo[end÷2, :, 1:3]; tank_coo[3*end÷4, :, 1:3]]
+    tank_plan = [reshape(tank_coo, n_circ * n_secs * 3, 3); tank_ske]
     @series begin
-        @views tank_plan[:,1], tank_plan[:,2], tank_plan[:,3]
+        @views tank_plan[:, 1], tank_plan[:, 2], tank_plan[:, 3]
     end
 end
