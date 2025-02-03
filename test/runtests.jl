a6b147e30ddfb57a83a1ce0ccf1055d9356b7952
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2025-02-03 16:52:16 +0000

    Removes erroring tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,6 @@ using Test
 
 @testset "AeroFuseHydrogen.jl" begin
     # Write your tests here.
-    @test AeroFuseHydrogen.greet_AeroFuse() == "Hello AeroFuseHydrogen"
-    @test AeroFuseHydrogen.greet_AeroFuse() != "Hello world!"
     @test AeroFuseHydrogen.CryogenicFuelTank() isa CryogenicFuelTank
 
     tank1 = CryogenicFuelTank(
