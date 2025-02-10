41cf2888fe68152e3d4d0df2a55ecebe7a4cc7f8
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2025-02-10 14:17:54 +0000

    Removes Type "A" from pemfc_polarization

--- src/Tools/FuelCellProperties.jl
+++ src/Tools/FuelCellProperties.jl
@@ -1,8 +1,7 @@
 module FuelCellProperties
 
-function pemfc_polarization(i::Vector{A}=LinRange(0, 1.5, 20), E_rTP::A=0.0, T::A=333.0, α::A=1.0, n::Integer=2, i_loss::A=20.0, i_0::A=0.03, i_L::A=16000.0, R_i::A=1500.0)
+function pemfc_polarization(i::Vector{Number}=LinRange(0, 1.5, 20), E_rTP=0.0, T=333.0, α=1.0, n::Integer=2, i_loss=20.0, i_0=0.03, i_L=16000.0, R_i=1500.0)
     @assert length(i) ≥ 1 "Current density vector must have at least one element"
-    A = promote_type(eltype(i)[1], eltype(T), eltype(α), eltype(i_loss), eltype(i_0), eltype(i_L), eltype(R_i))
 
     F = 96485.  # Faraday constant (C/mol)
     R = 8.314   # Ideal gas constant (J/mol/K)
