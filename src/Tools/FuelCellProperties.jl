958fe7dc6e33215ad0397b9099beda48ec94712b
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2025-02-10 14:56:39 +0000

    Adds preliminary E_cell plotting to notebook; adds blank comment to FuelCellProperties

--- src/Tools/FuelCellProperties.jl
+++ src/Tools/FuelCellProperties.jl
@@ -1,6 +1,9 @@
 module FuelCellProperties
 using Base: LinRange
 
+"""
+
+"""
 function pemfc_polarization(i, E_rTP::Number=0.0, T::Number=333.0, α::Number=1.0, n::Integer=2, i_loss::Number=20.0, i_0::Number=0.03, i_L::Number=16000.0, R_i::Number=1500.0)
     F = 96485.  # Faraday constant (C/mol)
     R = 8.314   # Ideal gas constant (J/mol/K)
