3f13954fc9e910192e6b094bde6f6fcedb166621
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2025-02-13 14:06:01 +0000

    Fixes error

--- src/Tools/FuelCellProperties.jl
+++ src/Tools/FuelCellProperties.jl
@@ -8,7 +8,7 @@ function pemfc_polarization(i::Number, T::Number=333.0, α::Number=1.0, n::Integ
     F = 96485.  # Faraday constant (C/mol)
     R = 8.314   # Ideal gas constant (J/mol/K)
 
-    E_rTP = 1.482 - 0.000845 * T + 0.0000431 * T * log(0.21)^0.5
+    E_rTP = 1.482 - 0.000845 * T + 0.0000431 * T * log(sqrt(0.21))
 
     print(E_rTP)
     print(- (R * T) / (α * F) * log((i + i_loss) / i_0))
