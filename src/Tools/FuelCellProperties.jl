7cba8c7511dbddb2901c89c4ef3334df9a5e2b82
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2025-02-13 13:57:41 +0000

    Adds E_rTP as a calculated value in pemfc_polarization

--- src/Tools/FuelCellProperties.jl
+++ src/Tools/FuelCellProperties.jl
@@ -4,12 +4,14 @@ using Base: LinRange
 """
 
 """
-function pemfc_polarization(i::Number, E_rTP::Number=0.0, T::Number=333.0, α::Number=1.0, n::Integer=2, i_loss::Number=20.0, i_0::Number=0.03, i_L::Number=16000.0, R_i::Number=1500.0)
+function pemfc_polarization(i::Number, T::Number=333.0, α::Number=1.0, n::Integer=2, i_loss::Number=20.0, i_0::Number=0.03, i_L::Number=16000.0, R_i::Number=1500.0)
     F = 96485.  # Faraday constant (C/mol)
     R = 8.314   # Ideal gas constant (J/mol/K)
 
+    E_rTP = 1.482 - 0.000845 * T + 0.0000431 * T * sqrt(log(0.21))
+
     E_cell = @. E_rTP - (R * T) / (α * F) * log((i + i_loss) / i_0) - (R * T) / (n * F) * log(i_L / (i_L - i)) - R_i * i
-# SEE PAGE 65 FOR EXAMPLE CALCULATION AAAA
+
     return E_cell
 end
 
