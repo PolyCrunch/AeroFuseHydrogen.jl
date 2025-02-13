c2738e6447d43b79f3a4bb02b94e77777c18ec44
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2025-02-13 14:14:36 +0000

    Adds docs to pemfc_polarization; finishes function

--- src/Tools/FuelCellProperties.jl
+++ src/Tools/FuelCellProperties.jl
@@ -2,18 +2,34 @@ module FuelCellProperties
 using Base: LinRange
 
 """
+pemfc_polarization(
+    i::Number, 
+    T::Number=333.0, 
+    α::Number=1.0, 
+    n::Integer=2, 
+    i_loss::Number=0.002, 
+    i_0::Number=3e-6, 
+    i_L::Number=1.6, 
+    R_i::Number=0.15
+)
 
+Compute the cell potential difference of a H2:Air proton exchange membrane fuel cell.
+
+# Arguments
+- `i::Number`: Current density (A/cm²)
+- `T::Number=333.0`: Temperature (K)
+- `α::Number=1.0`: Charge transfer coefficient
+- `n::Integer=2`: Number of electrons involved
+- `i_loss::Number=0.002`: Current loss (A/cm²)
+- `i_0::Number=3e-6`: Reference exchange current density (A/cm²)
+- `i_L::Number=1.6`: Limiting current density (A/cm²)
+- `R_i::Number=0.15`: Internal resistance (Ω/cm²)
 """
-function pemfc_polarization(i::Number, T::Number=333.0, α::Number=1.0, n::Integer=2, i_loss::Number=20.0, i_0::Number=0.03, i_L::Number=16000.0, R_i::Number=1500.0)
-    F = 96485.  # Faraday constant (C/mol)
+function pemfc_polarization(i::Number, T::Number=333.0, α::Number=1.0, n::Integer=2, i_loss::Number=0.002, i_0::Number=3e-6, i_L::Number=1.6, R_i::Number=0.15)
+    F = 96485.0  # Faraday constant (C/mol)
     R = 8.314   # Ideal gas constant (J/mol/K)
 
     E_rTP = 1.482 - 0.000845 * T + 0.0000431 * T * log(sqrt(0.21))
-
-    print(E_rTP)
-    print(- (R * T) / (α * F) * log((i + i_loss) / i_0))
-    print(- (R * T) / (n * F) * log(i_L / (i_L - i)))
-    print(- R_i * i)
 
     E_cell = @. E_rTP - (R * T) / (α * F) * log((i + i_loss) / i_0) - (R * T) / (n * F) * log(i_L / (i_L - i)) - R_i * i
 
