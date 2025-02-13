6f6e1dc502679d90eab96f3879cb121394dd8d0f
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2025-02-13 14:02:41 +0000

    Adds prints for debugging in pemfc_polarization

--- src/Tools/FuelCellProperties.jl
+++ src/Tools/FuelCellProperties.jl
@@ -10,6 +10,11 @@ function pemfc_polarization(i::Number, T::Number=333.0, α::Number=1.0, n::Integ
 
     E_rTP = 1.482 - 0.000845 * T + 0.0000431 * T * log(0.21)^0.5
 
+    print(E_rTP)
+    print(- (R * T) / (α * F) * log((i + i_loss) / i_0))
+    print(- (R * T) / (n * F) * log(i_L / (i_L - i)))
+    print(- R_i * i)
+
     E_cell = @. E_rTP - (R * T) / (α * F) * log((i + i_loss) / i_0) - (R * T) / (n * F) * log(i_L / (i_L - i)) - R_i * i
 
     return E_cell
