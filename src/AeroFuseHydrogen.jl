fba457e29477c4b829a1b5e09f81e19c88d85ff6
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2025-02-14 15:38:12 +0000

    Adds \eta_FC as exports

--- src/AeroFuseHydrogen.jl
+++ src/AeroFuseHydrogen.jl
@@ -47,9 +47,9 @@ import .HydrogenElectric: tank_surface_temperature, boil_off
 export tank_surface_temperature, boil_off
 
 # Fuel cell
-import .HydrogenElectric: PEMFCStack, j_cell, U_cell
+import .HydrogenElectric: PEMFCStack, j_cell, U_cell, η_FC
 
-export PEMFCStack, j_cell, U_cell
+export PEMFCStack, j_cell, U_cell, η_FC
 
 
 ## Post-processing
