0db64d185309ac8dbf1b556c8790cc378772c6dd
Tom Gordon <53229452+PolyCrunch@users.noreply.github.com>
2025-04-23 15:20:10 +0100

    Modifies comments in psfc

--- src/HydrogenElectric/propulsion.jl
+++ src/HydrogenElectric/propulsion.jl
@@ -3,7 +3,7 @@ psfc(A_eff::Number=120., P::Number=1.e6)
 """
 function psfc(A_eff=120.::Number, P::Number=1.e6)
     stack = PEMFCStack(area_effective=A_eff, power_max=P); # Create a PEMFCStack object with default parameters
-    m_dot = fflow_H2(stack, 1.) # Calculate the mass flow rate of hydrogen (kg/s)
+    m_dot = fflow_H2(stack, 1.) # Calculate the mass flow rate of hydrogen at full throttle (worst case) [kg/s]
 
-    return m_dot / P # Calculate the power specific fuel consumption (kg/W)
+    return m_dot / P # Calculate the power specific fuel consumption [kg/W]
 end
